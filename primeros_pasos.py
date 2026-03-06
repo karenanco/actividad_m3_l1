e5d2e1757ba0e0b452afb4b6f669d41fdc18b488
Karen Ñanco <k.nanco.vasquez@gmail.com>
2026-03-06 01:21:42 +0000

    primeros pasos

---
+++ primeros_pasos.py
@@ -0,0 +1,11 @@
+#mensaje inicial
+print ("¡Hola! Este es mi primer programa en Python." )
+
+#variables simples
+nombre = "Karen Ñanco Vásquez"
+edad = 35
+ciudad = "Osorno"
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
